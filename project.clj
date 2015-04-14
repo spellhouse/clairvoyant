d08c9d821dc002031ca16b07cf8c8de9d5c1046b
lorddoig <sean.doig@gmail.com>
2015-04-14 19:50:46 +0100

    Prettify project.clj

--- project.clj
+++ project.clj
@@ -17,37 +17,57 @@
   [[org.clojure/clojure "1.6.0" :scope "provided"]
    [org.clojure/clojurescript "0.0-2322" :scope "provided"]]
 
-  :clean-targets ^{:protect false} ["dev-resources/public/out" "resources/public/out" "target"]
+  :clean-targets 
+  ^{:protect false} ["dev-resources/public/out"
+                     "resources/public/out"
+                     "target"]
 
-  :cljsbuild {:builds {:app {:source-paths ["src"]
-                             :compiler     {:output-to     "dev-resources/public/clairvoyant.js"
-                                            :output-dir    "dev-resources/public/out/"
-                                            :optimizations :none
-                                            :pretty-print  true}}}}
+  :cljsbuild 
+  {:builds 
+   {:app {:source-paths ["src"]
+          :compiler     
+          {:output-to "dev-resources/public/clairvoyant.js"
+           :output-dir "dev-resources/public/out/"
+           :optimizations :none
+           :pretty-print true}}}}
 
   :aliases
   {"auto-build" ~(clojure.string/split
                    "do cljsbuild clean, cljsbuild auto"
                    #"\s+")}
 
-  :release-tasks [["clean"]
-                  ["with-profiles" "-dev,+release" "cljsbuild" "once"]
-                  ["jar"]]
+  :release-tasks 
+  [["clean"]
+   ["with-profiles" 
+    "-dev,+release" 
+    "cljsbuild" "once"]
+   ["jar"]]
 
   :profiles
-  {:dev     {:dependencies [[com.cemerick/piggieback "0.1.3"]
-                            [weasel "0.4.0-SNAPSHOT"]]
+  {:dev     
+   {:dependencies 
+    [[com.cemerick/piggieback "0.1.3"]
+     [weasel "0.4.0-SNAPSHOT"]]
 
-             :plugins      [[lein-cljsbuild "1.0.3"]]
+    :plugins      
+    [[lein-cljsbuild "1.0.3"]]
 
-             :source-paths ["dev"]
+    :source-paths 
+    ["dev"]
 
-             :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+    :repl-options 
+    {:nrepl-middleware 
+     [cemerick.piggieback/wrap-cljs-repl]}
 
-             :cljsbuild    {:builds {:app {:source-paths ["dev"]}}}}
+    :cljsbuild
+    {:builds {:app {:source-paths ["dev"]}}}}
 
-   :release {:cljsbuild {:jar    true
-                         :builds {:app {:compiler {:output-to     "resources/public/clairvoyant.js"
-                                                   :output-dir    "resources/public/out/"
-                                                   :optimizations :advanced
-                                                   :pretty-print  false}}}}}})
+   :release 
+   {:cljsbuild 
+    {:jar true
+     :builds 
+     {:app {:compiler 
+            {:output-to "resources/public/clairvoyant.js"
+             :output-dir    "resources/public/out/"
+             :optimizations :advanced
+             :pretty-print  false}}}}}})
