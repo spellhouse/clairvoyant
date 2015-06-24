7c7638224438d31f0c932561caa602995c0f722e
Stuart Mitchell <github@stuartmitchell.com>
2015-06-24 16:57:52 +1200

    removed make-tracer

--- src/clairvoyant/core.cljs
+++ src/clairvoyant/core.cljs
@@ -60,42 +60,23 @@
       (list 'fn (symbol name) arglist)
       (list 'fn arglist))))
 
-(defn make-tracer
-  "makes a tracer given parameters
-    - str-fn if true uses pr-str to format values
-    - log-to-console if true also logs values directly to console
-    - shorten-fns if true functions are found and shortened for printing"
-  [str-fn log-to-console shorten-fns]
+(def default-tracer
   (let [pr-val* (fn pr-val* [x]
                   (cond 
                     (fn? x) 
-                    (if shorten-fns 
-                      (fn-signature x)
-                      x)
+                    (fn-signature x)
                     (coll? x)
                     (walk pr-val* identity x)
                     :else x))
-        pr-val (fn [x] (if str-fn
-                         (pr-str (pr-val* x))
-                         (pr-val* x)))
+        pr-val (fn [x] (pr-str (pr-val* x)))
         log-binding (fn [form init]
-                      (if str-fn 
-                        (.groupCollapsed js/console "%c%s %c%s"
-                                         "font-weight:bold;"
-                                         (pr-str form)
-                                         "font-weight:normal;"
-                                         (pr-val init))
-                        (.groupCollapsed js/console "%c%s"
-                                         "font-weight:bold;"
-                                         (pr-str form)
-                                         (pr-val init))))
+                        (.groupCollapsed js/console "%c%s %c%s" 
+                                            "font-weight:bold;" (pr-str form)
+                                            "font-weight:normal;" (pr-val init)))
         log-exit (fn [exit]
-                   (if log-to-console
-                     (do
                        (.groupCollapsed js/console "=>" (pr-val exit))
                        (.log js/console exit)
                        (.groupEnd js/console))
-                     (.log js/console "=>" (pr-val exit))))
         has-bindings? #{'fn*
                         `fn
                         'fn
@@ -178,8 +159,90 @@
                  (.groupEnd js/console)))
              (.groupEnd js/console)))))))
 
-(def default-tracer 
-  (make-tracer true true true))
-
 (def cljs-devtools-tracer 
-  (make-tracer false false false))
+  (let [pr-val (fn pr-val [x] x)
+        log-binding (fn [form init]
+                      (.groupCollapsed js/console "%c%s"
+                                        "font-weight:bold;" (pr-str form)
+                                        (pr-val init)))
+        log-exit (fn [exit]
+                   (.log js/console "=>" (pr-val exit)))
+        has-bindings? #{'fn*
+                        `fn
+                        'fn
+                        'defn
+                        `defn
+                        'defmethod
+                        `defmethod
+                        'deftype
+                        `deftype
+                        'defrecord
+                        `defrecord
+                        'reify
+                        `reify
+                        'let
+                        `let
+                        'extend-type
+                        `extend-type
+                        'extend-protocol
+                        `extend-protocol}
+        fn-like? (disj has-bindings? 'let `let)]
+    (reify
+      ITraceEnter
+      (-trace-enter
+        [_ {:keys [anonymous? arglist args dispatch-val form init name ns op protocol]}]
+        (cond
+          (fn-like? op)
+          (let [title (if protocol
+                        (str protocol " " name " " arglist)
+                        (str ns "/" name
+                             (when dispatch-val
+                               (str " " (pr-str dispatch-val)))
+                             (str " " arglist)
+                             (when anonymous? " (anonymous)")))
+                arglist (remove '#{&} arglist)]
+            (.groupCollapsed js/console title)
+            (.groupCollapsed js/console "bindings"))
+          
+          (#{'let `let} op)
+          (let [title (str op)]
+            (.groupCollapsed js/console title)
+            (.groupCollapsed js/console "bindings"))
+          
+          (#{'binding} op)
+          (log-binding form init)))
+      
+      ITraceExit
+      (-trace-exit [_ {:keys [op exit]}]
+                   (cond
+                     (#{'binding} op)
+                     (do (log-exit exit)
+                       (.groupEnd js/console))
+                     
+                     (has-bindings? op)
+                     (do (.groupEnd js/console)
+                       (log-exit exit)
+                       (.groupEnd js/console))))
+      
+      ITraceError
+      (-trace-error [_ {:keys [op form error ex-data]}]
+                    (cond
+                      (#{'binding} op)
+                      (do
+                        (.error js/console (.-stack error))
+                        (when ex-data
+                          (.groupCollapsed js/console "ex-data")
+                          (.groupCollapsed js/console (pr-val ex-data))
+                          (.groupEnd js/console)
+                          (.groupEnd js/console)))
+                      
+                      (has-bindings? op)
+                      (do (.groupEnd js/console)
+                        (do
+                          (.error js/console (.-stack error))
+                          (when ex-data
+                            (.groupCollapsed js/console "ex-data")
+                            (.groupCollapsed js/console (pr-val ex-data))
+                            (.groupEnd js/console)
+                            (.groupEnd js/console)))
+                        (.groupEnd js/console)))))))
